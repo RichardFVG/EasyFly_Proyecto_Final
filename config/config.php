3eb8840fc6e066c4a2640c3e4ef643de9c4d2008
RichardFVG <richi3fvg@gmail.com>
2025-05-17 16:14:57 +0100

    n4

--- config/config.php
+++ config/config.php
@@ -9,5 +9,5 @@ define('DB_PASS', '');
 // API KEYS (rellenar con tus claves reales)
 define('MAILJET_API_PUBLIC', 'b202e7778b9637602c4d8fb04b91c499');
 define('MAILJET_API_PRIVATE', '80c87ad5670a6b953db04a462f117eef');
-define('OPENWEATHER_KEY', 'TU_API_OPENWEATHER');
+define('OPENWEATHER_KEY', 'e99acd182225c930633145d376acb28e');
 ?>
