8f4bd2d2157b4f8929bd9e1055883c0989cca34a
RichardFVG <richi3fvg@gmail.com>
2025-05-17 22:15:03 +0100

    n6

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,5 +1,11 @@
 
 </div> <!-- container -->
+<hr>
+<footer class="footer">
+    <p>© EasyFly, Richard Francisco Vaca Garcia, 
+    2do CFGS DAW</p>
+</footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- views/partials/header.php
+++ views/partials/header.php
@@ -1,3 +1,4 @@
+
 <?php 
 require_once __DIR__ . '/../../helpers/Weather.php';
 Auth::start();
