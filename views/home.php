0fbb52b06721cee577355fd3d75240ca99a53f3a
RichardFVG <richi3fvg@gmail.com>
2025-05-23 07:40:48 +0100

    n25

--- views/home.php
+++ views/home.php
@@ -1,7 +1,7 @@
 <?php include __DIR__.'/partials/header.php'; ?>
 
 <div class="jumbotron text-center">
-    <h1 class="display-5">Bienvenido a EasyFly</h1>
+    <h1 class="display-5">Bienvenidos a EasyFly</h1>
     <p class="lead">Reserva vuelos directos de forma sencilla.</p>
 
     <a class="btn btn-success" href="default.php?controller=flight&action=list">
